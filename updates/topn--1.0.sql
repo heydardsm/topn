4f325c3cf84e54b2b5741549561e43c544889cf0
h.dasomi <heydar@maxduel.io>
2026-03-19 11:14:58 +0330

    fixed topn, return correct data

--- updates/topn--1.0.sql
+++ updates/topn--1.0.sql
@@ -293,12 +293,19 @@ DECLARE
 BEGIN
     table_name := 'topn_' || namespace;
     RETURN QUERY EXECUTE format('
-        SELECT key, frequently
-        FROM %I
-        WHERE uid = %L
-        ORDER BY frequently DESC
-        LIMIT %s',
-        table_name, p_uid, n
+        WITH top_values as (
+            SELECT DISTINCT(frequently) as frequently
+            FROM %I
+            WHERE uid = %L
+            ORDER BY frequently DESC
+            LIMIT %s
+        )
+        SELECT t.key as key, t.frequently as frequently
+        FROM %I t
+        INNER JOIN top_values topv ON topv.frequently = t.frequently
+        WHERE t.uid = %L
+        ORDER BY frequently DESC',
+        table_name, p_uid, n, table_name, p_uid
     );
 END;
 $$;
